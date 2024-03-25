86a266ab9b6ccaccae26fcf0a8396e14f08a2a0b
Richard Montesdeoca <desarrollos4@ambacar.com>
2024-03-25 10:15:55 -0500

    profile concert

--- MusicStore.Services/Profiles/ConcertProfile.cs
+++ MusicStore.Services/Profiles/ConcertProfile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using MusicStore.Dto.Request;
+using MusicStore.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,18 @@ using System.Threading.Tasks;
 
 namespace MusicStore.Services.Profiles
 {
-    public class ConcertProfile
+    public class ConcertProfile: Profile
     {
-
+        public ConcertProfile()
+        {
+            CreateMap<ConcertDtoRequest, Concert>()
+                .ForMember(destino => destino.GenderId, origen => origen.MapFrom(x => x.IdGender))
+                .ForMember(destino => destino.Title, origen => origen.MapFrom(x => x.Title))
+                .ForMember(destino => destino.Description, origen => origen.MapFrom(x => x.Description))
+                .ForMember(destino => destino.TicketsQuantity, origen => origen.MapFrom(x => x.TicketsQuantity))
+                .ForMember(destino => destino.UnitPrice, origen => origen.MapFrom(x => x.UnitPrice))
+                .ForMember(destino => destino.Place, origen => origen.MapFrom(x => x.Place));
+                
+        }
     }
 }
